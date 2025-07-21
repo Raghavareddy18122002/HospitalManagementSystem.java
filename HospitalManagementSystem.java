54d5453844388dc79179aa270defd17d8ba5d7d0
AdiRaghava <165063040+Raghavareddy18122002@users.noreply.github.com>
2025-07-21 15:07:33 +0530

    Update HospitalManagementSystem.java

--- HospitalManagementSystem.java
+++ HospitalManagementSystem.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class HospitalManagementSystem {
 
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
-    private static final String username ="root";
-    private static final String password = "6305570042";
+    private static final String username ="raghava";
+    private static final String password = "9392647721";
     public static void main(String[] args) {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
